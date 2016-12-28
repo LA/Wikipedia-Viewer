290de90810c40bf11ed09a29e1c593cfac89174b
Adar Butel <theadarbutel@gmail.com>
2016-12-28 12:51:36 -0800

    Added Javascript Documentation

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,10 @@ function wikiSearch(input) {
     var url = "https://en.wikipedia.org/w/api.php" +
 "?action=opensearch" +
 "&search=" + input +
-"&limit=15" +
+"&limit=15" + // 15 results
 "&namespace=0" +
 "&format=json" +
-"&origin=*";
+"&origin=*"; // <-- This is necessary for no browser errors.
 
     // Request Wiki API
     $.getJSON(url, function(data) {
@@ -18,18 +18,27 @@ function wikiSearch(input) {
 function displaySearchResults(data) {
     var html = "";
     var results = 15;
+
+    // Starting number to index data
     var i = 1;
 
+    // For each result grab the title, description, and url
     for (var j=0; j < results; j++) {
+        // Save link for replacing with URL
         var link = "article-" + j;
+        // Create anchor tags to hold the link
         html += "<a href=\"" + link + "\"" + "target=\"_blank\"" + ">"
         html += "<div class=\"result-box\">"
+        // Add Title
         html += "<h3>" + cleanedData(data[i][j]) + "</h3>";
         i += 1;
+        // Add Description
         html += "<p>" + cleanedData(data[i][j]) + "</p>";
         i += 1;
+        // Replace 'link' with the actual URL
         html = html.replace(link, cleanedData(data[i][j]));
         html += "</div></a>";
+        // Set i back to 1 for the next result
         i = 1;
     }
 
